d545ea0c42614e7fef161d69915a319f1f93094e
jaralusman888-arch <jaralusman888@gmail.com>
2026-08-24 19:55:55 +0500

    Implement App component with landing page and product list

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ProductList from './ProductList';
+import './App.css';
+import AboutUs from './AboutUs';
+
+function App() {
+  const [showProductList, setShowProductList] = useState(false);
+
+  const handleGetStartedClick = () => {
+    setShowProductList(true);
+  };
+
+  return (
+    <div className="app-container">
+      {!showProductList ? (
+        <div className="landing-page">
+          <div className="background-image"></div>
+          <div className="content">
+            <h1>Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+          <AboutUs />
+        </div>
+      ) : (
+        <ProductList />
+      )}
+    </div>
+  );
+}
+
+export default App;
